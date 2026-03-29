015f883f53bdca1b807440ed811270029785a536
Frederick Shawgrove <alphatron150@gmail.com>
2026-03-29 00:35:21 +0000

    Add files

--- ML/SimonPullen/src/backtesting/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backtesting framework using Simon's bar replay method"""
-from .bar_replay import BarReplay
-from .trade_logger import TradeLogger
-from .performance_analyzer import PerformanceAnalyzer
-from .strategy_comparator import StrategyComparator
-
-__all__ = ['BarReplay', 'TradeLogger', 'PerformanceAnalyzer', 'StrategyComparator']
